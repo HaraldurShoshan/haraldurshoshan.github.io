b58b40eb7b127aa52e2fde30d653687814adc737
HaraldurShoshan <haraldurshoshan@gmail.com>
2016-11-05 14:27:23 +0000

    changes to index

--- js/shoshan.js
+++ js/shoshan.js
@@ -1,12 +1,31 @@
 $(document).ready(function(){
     //$('.modal').modal();
-    $('.modal-trigger').leanModal({
-        dismissible: false
+
+ //    $('.modal-trigger').leanModal({
+ //        dismissible: false
+ //    });
+	// $('.close').click(function () {
+ //  		//$('#videoModalone').hide();
+ //  		$('#videoModalone iframe').attr("src", jQuery("#videoModalone iframe").attr("src"));
+	// });
+	$('.halliBass')
+    .on('click', function(){
+        var div = $(this);
+        div.stop(true, true).animate({ 
+            margin: -30,
+            width: "+=100",
+            height: "+=100"
+        }, 'fast');
+    })
+    .on('mouseleave', function(){
+        var div = $(this);
+        div.stop(true, true).animate({ 
+            margin: 0,
+            width: "-=100",
+            height: "-=100"
+        }, 'fast');
     });
-	$('.close').click(function () {
-  		//$('#videoModalone').hide();
-  		$('#videoModalone iframe').attr("src", jQuery("#videoModalone iframe").attr("src"));
-	});
+
 
 	$(".button-collapse").sideNav();
 });
